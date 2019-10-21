05c16b9adfe53dddf2db2f529fdf5767c13d89fb
Andreas <andreas@eclecticiq.com>
2019-10-21 14:10:02 +0200

    fix ownership, setup.py, gitlab -> github (#1)

--- README.md
+++ README.md
@@ -4,7 +4,5 @@ markdown\_rundoc
 [![PyPI version](https://badge.fury.io/py/markdown_rundoc.svg)](https://badge.fury.io/py/markdown_rundoc)
 [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
 
-**This is only a mirror. Please see the [official repository on GitLab](https://gitlab.com/nul.one/markdown-rundoc)**
-
 Markdown extensions used by rundoc project.
 
--- setup.py
+++ setup.py
@@ -1,47 +1,45 @@
 #!/usr/bin/env python3
 
-import markdown_rundoc as project
-
 from setuptools import setup, find_packages
 import os
+
 
 def here(*path):
     return os.path.join(os.path.dirname(__file__), *path)
 
+
 def get_file_contents(filename):
-    with open(here(filename), 'r', encoding='utf8') as fp:
+    with open(here(filename), "r", encoding="utf8") as fp:
         return fp.read()
 
+
 setup(
-    name = project.__name__,
-    description = project.__doc__.strip(),
-    long_description=get_file_contents('README.md'),
-    long_description_content_type='text/markdown',
-    url = 'https://gitlab.com/nul.one/' + project.__name__,
-    download_url = 'https://gitlab.com/nul.one/{1}/-/archive/{0}/{1}-{0}.tar.gz'.format(project.__version__, project.__name__),
-    version = project.__version__,
-    author = project.__author__,
-    author_email = project.__author_email__,
-    license = project.__license__,
-    packages = [ project.__name__ ],
+    name="markdown_rundoc",
+    description="Markdown extensions for Rundoc.",
+    long_description=get_file_contents("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/eclecticiq/markdown-rundoc",
+    version="0.3.1",
+    author="EclecticIQ",
+    author_email="opensource@eclecticiq.com",
+    license="BSD",
+    packages=find_packages(include=["markdown_rundoc", "markdown_rundoc.*"]),
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Documentation',
-        'Topic :: Software Development :: Documentation',
-        'Topic :: Software Development :: Testing',
-        'Topic :: Utilities',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Customer Service",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
     ],
-    install_requires = [
-        'markdown>=2.6.9,<3.0',
-    ],
+    install_requires=["markdown>=2.6.9,<3.0"],
     python_requires=">=3.4.6",
 )
-
