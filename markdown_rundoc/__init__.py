6b9a41895bfd62c3ba160bf2bd1064422d2b4197
Predrag Mandic <predrag@nul.one>
2019-11-27 13:50:43 +0100

    ownership fix

--- markdown_rundoc/__init__.py
+++ markdown_rundoc/__init__.py
@@ -4,9 +4,9 @@ Set of markdown extensions used by rundoc utility.
 
 __version__ = "0.3.1"
 __license__ = "BSD"
-__year__ = "2018"
-__author__ = "Predrag Mandic"
-__author_email__ = "predrag@eclecticiq.com"
+__year__ = "2018-2019"
+__author__ = "Predrag Mandic, EclecticIQ"
+__author_email__ = "rundoc@eclecticiq.com"
 __copyright__ = "Copyright {} {} <{}>".format(
     __year__, __author__, __author_email__)
 
